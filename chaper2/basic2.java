c751aa1edfc6de95b04f9482bf53f6deedd6e040
mahfoozalamcse <amahfooz1007@gmail.com>
2026-04-10 23:55:10 +0530

    practice basic part 2

--- chaper2/basic2.java
+++ chaper2/basic2.java
@@ -2,6 +2,27 @@ package chaper2;
 
 public class basic2 {
     public static void main(String[] args) {
+     
+      int day = 2;
+      switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
         
+        default:
+            break;
+      }
+
+
     }
+
 }
