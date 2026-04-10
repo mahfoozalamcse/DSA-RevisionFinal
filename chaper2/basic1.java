826ca9936ac469d733fc989fd05f5be108b52dbb
mahfoozalamcse <amahfooz1007@gmail.com>
2026-04-10 21:57:31 +0530

    practice basic

---
+++ chaper2/basic1.java
@@ -0,0 +1,51 @@
+package chaper2;
+
+public class basic1 {
+    public static void main(String[] args) {
+        // variable
+        int a = 10;
+        a = 20; // reasign posible only
+
+        System.out.println(" ans : "+ a);
+
+        // single line comment
+        // 
+        // 
+        // 
+        /*
+        multi line comment
+        */
+
+        
+        int b = 20;
+
+        /*
+        Non primitive
+        String
+        Array
+        Classes
+        Interfaces
+        Objects
+        */
+       
+        
+
+        
+        
+        System.out.println(b);
+
+
+
+        String name = " Mahfooz";
+        System.out.println(name);
+
+        
+        int[] arr = {1, 2, 3, 4};
+
+       for (int i = 0; i < arr.length; i++) {
+           System.out.print(" "+arr[i]);
+       }
+        
+    }
+}
+
---
+++ chaper2/basic2.java
@@ -0,0 +1,7 @@
+package chaper2;
+
+public class basic2 {
+    public static void main(String[] args) {
+        
+    }
+}
